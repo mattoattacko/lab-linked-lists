e9df664820941c242cc54806bb8316bf59faf794
Matto <matt.mcquain@gmail.com>
2019-02-23 19:53:12 -0800

    organizational fixes

--- __tests__/testing.linkedlist.js
+++ __tests__/testing.linkedlist.js
@@ -3,28 +3,23 @@
 let LL = require('../lib/linkedlist.js');
 
 describe('Linked List', () => {
-
   it('constructor()', ()=> {
-    let list = new LL();
-    expect(list.head).toBeNull();
+    let linkedList = new LL();
+    expect(linkedList.head).toBeNull();
   });
-
+  
   it('append()', ()=> {
-    let list = new LL();
-    let initialValue = 'First Phrase';
-    list.append(initialValue);
-    expect(list.head.value).toEqual(initialValue);
-
-    let newValue = 'Second Saying';
-    list.append(newValue);
-    expect(list.head.value).toEqual(initialValue);
-
-    let anotherOne = 'Third Thought';
-    list.append(anotherOne);
-    expect(list.head.value).toEqual(initialValue);
-
-    console.log(list);
-    
+    let linkedList = new LL();
+    let startValue = 'First Value';
+    linkedList.append(startValue);
+    expect(linkedList.head.value).toEqual(startValue);
+    let secondValue = 'Second Value';
+    linkedList.append(secondValue);
+    expect(linkedList.head.value).toEqual(startValue);
+    let thirdValue = 'Third Value';
+    linkedList.append(thirdValue);
+    expect(linkedList.head.value).toEqual(startValue);
+    console.log(linkedList);
   });
 
   it('prepend()', () => {
@@ -32,17 +27,12 @@ describe('Linked List', () => {
     let initialValue = 'First Phrase';
     list.prepend(initialValue);
     expect(list.head.value).toEqual(initialValue);
-
     let newValue = 'Second Saying';
     list.prepend(newValue);
     expect(list.head.value).toEqual(newValue);
-
     let anotherOne = 'Third Thought';
     list.prepend(anotherOne);
     expect(list.head.value).toEqual(anotherOne);
-
     console.log(list);
-
   });
-
 });
--- index.js
+++ index.js
@@ -14,21 +14,18 @@ class LinkedList {
 
   append(value) {
     let node = new Node(value);
-
     if(! this.head) {
       this.head = node;
       return this;
     }
-
     let currentNode = this.head;
     while(currentNode.next) {
       currentNode = currentNode.next;
     }
-
     currentNode.next = node;
     return this;
   }
 }
 
-let list = new LinkedList();
-console.log(list);
+let ourList = new LinkedList();
+console.log(ourList);
